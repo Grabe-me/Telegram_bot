513a03d61d2a3f286285584f06d49cb3d817ca5b
Grabe-me <grabe85me@gmail.com>
2023-02-28 14:01:14 +0300

    Fixing bugs 001

--- main.py
+++ main.py
@@ -8,14 +8,19 @@ dp = Dispatcher(bot)
 import random
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-     await message.answer('Привет! Напиши, сколько символов должен быть пароль, максимум 72 символа.')
+    await message.answer(f'Привет! Напиши, сколько символов'
+                         'должен быть пароль, максимум 72 символа.')
 @dp.message_handler()
 async def get_password(message: types.Message):
     passlength = message.text
     try:
         passlength = int(passlength)
-        if passlength < 0 and passlength > 72:
-            await message.reply('Недопустимый размер пароля')
+        if passlength <= 0:
+            await message.reply('Пароль не может быть меньше 0')
+            await message.answer('Попробуй любое целое число от 1 до 72')
+        elif passlength > 72:
+            await message.reply('Слишком длинный пароль')
+            await message.answer('Попробуй любое целое число от 1 до 72')
         else:
             await message.answer('Вот 5 вариантов пароля:')
             a = 'abcdefghijklmnopqrstuvwxyz'
@@ -27,9 +32,12 @@ async def get_password(message: types.Message):
                 password = ''.join(random.sample(all, passlength))
                 await message.answer(f"\t{password}")
                 start = f"@dp.message_handler(commands=['start'])"
+            if passlength < 6:
+                await message.answer('Пароль меньше 6 символов считается "слабым"')
             await message.answer('Можем попробовать снова. Просто нажми /start')
     except Exception as ex2:
         print(ex2)
-        await message.answer('Необходимо ввести подходящее число')
+        await message.reply('Это не подойдет')
+        await message.answer('Попробуй любое целое число от 1 до 72')
 if __name__ == '__main__':
     executor.start_polling(dp)
